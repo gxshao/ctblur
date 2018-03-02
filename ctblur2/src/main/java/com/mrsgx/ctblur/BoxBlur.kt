d762534a8a04decb4d9ef3f150fb851b41c7e8f0
shao <shao@mrsgx.com>
2018-03-02 14:27:48 +0800

    参数优化

--- ctblur2/src/main/java/com/mrsgx/ctblur/BoxBlur.kt
+++ ctblur2/src/main/java/com/mrsgx/ctblur/BoxBlur.kt
@@ -9,16 +9,19 @@ import android.graphics.Canvas
 
 class BoxBlur : IBlur {
     override fun blur(radius: Int, original: Bitmap): Bitmap {
+        var tmpRadius=radius
+        if (tmpRadius and 1 == 0)
+            tmpRadius+=1
 
         val blurred = Bitmap.createBitmap(original.width, original.height,
                 Bitmap.Config.ARGB_8888)
-        if (radius and 1 == 0) return blurred
+
         val c = Canvas(blurred)
         val w = original.width
         val h = original.height
         val pixels = IntArray(original.width * original.height)
         original.getPixels(pixels, 0, w, 0, 0, w, h)
-        c.drawBitmap(boxBlur(pixels,w,h,radius/2), 0, w, 0.0f, 0.0f, w, h, true, null)
+        c.drawBitmap(boxBlur(pixels,w,h,tmpRadius/2), 0, w, 0.0f, 0.0f, w, h, true, null)
         return blurred
     }
     private external fun boxBlur(pixels: IntArray,w:Int,h: Int,radius: Int):IntArray
--- ctblur2/src/main/java/com/mrsgx/ctblur/CTBlurData.kt
+++ ctblur2/src/main/java/com/mrsgx/ctblur/CTBlurData.kt
@@ -4,16 +4,17 @@ import android.graphics.Bitmap
 import android.view.View
 
 /**
- * 此类是渲染图像所需的一部分参数
- * 1.缩放比例大小
- * 2.未渲染原始View控件
- * 3.要附加的控件列表
- * 4.Bitmap位图配置
- * 5.渲染半径
- * 6.渲染算法
- * 7.加载脚本
- * Created by Marshall  on 2018/2/23.
  *
+ * 此类是渲染图像所需的一部分参数。<BR/>
+ * 1.缩放比例大小<BR/>
+ * 2.未渲染原始View控件<BR/>
+ * 3.要附加的控件列表<BR/>
+ * 4.Bitmap位图配置<BR/>
+ * 5.渲染半径<BR/>
+ * 6.渲染算法<BR/>
+ * 7.加载脚本<BR/>
+ * Created by Marshall  on 2018/2/23.
+ * <p>
  */
 class CTBlurData {
     var downSampleSize = 5
